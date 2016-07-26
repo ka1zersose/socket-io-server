b1dfd101833ffe107a8a11ba77cd414ed9dc9708
ka1zersose <kaizersose@gmail.com>
2016-07-26 16:45:04 +0300

    small fix

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var io = require('socket.io')(8086);
+var app = require('http').createServer(function (req, res){})
+var io = require('socket.io')(app, {
+   serveClient: false,
+   transport: ['websocket'],
+   pingTimeout: 100  * 1000,
+   pingInterval: 40 * 1000
+});
+
+app.listen(8086);
 
 io.on('connection', function (socket) {
     console.log('connected');
-    io.emit('this', { will: 'be received by everyone'});
+    setTimeout(() => {
+      for (var i = 0; i < 10000; i++){
+         io.emit('this', { will: 'be received by everyone'});
+      }
+   }, 1000 * 5);
+
 
     socket.on('private message', function (from, msg) {
         console.log('I received a private message by ', from, ' saying ', msg);
